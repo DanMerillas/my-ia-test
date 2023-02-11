ca2a07a659cd10b5c8ba190349d14647ae403b94
Daniel Merillas <dmerillasg@minsait.com>
2023-02-11 16:25:50 +0100

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
+import LoadingSpinner from './LoadingSpinner';
+
 
 function App() {
+
+  const [response, setResponse] = useState("");
+  const [responseImagen, setResponseImagen] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [inputTextImage, setinputTextImage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+
+  async function fetchData() {
+    setLoading(true)
+    const res = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ''
+      },
+      body: JSON.stringify({
+        prompt: inputText,
+        model: "text-davinci-003",
+        max_tokens: 4000,
+        temperature: 0
+      })
+    });
+    const data = await res.json();
+    setLoading(false)
+    setResponse(data.choices[0].text);
+
+  }
+
+  async function fetchDataImagen() {
+    setLoading(true)
+    const res = await fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : ''
+      },
+      body: JSON.stringify({
+        prompt: inputTextImage,
+        n: 1,
+        size: "512x512"
+      })
+    });
+    const data = await res.json();
+    setLoading(false)
+    setResponseImagen(data.data[0].url);
+  }
+
+  const handleKeyDown = (event:any)=>{
+    if (event.key === 'Enter') {
+      fetchData();
+    }
+  } 
+  const handleKeyDownImagen = (event:any)=>{
+    if (event.key === 'Enter') {
+      fetchDataImagen();
+    }
+  } 
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {loading ? <LoadingSpinner /> : <>
+      <h1>Preguntas:</h1>
+      <input type={'text'} onChange={(e) => setInputText(e.target.value)} onKeyDown={handleKeyDown} /><input type={'button'} value="Enviar" onClick={fetchData} />
+      {response !== "" && <><h1>Repuesta:</h1><p>{response}</p></>}
+
+      <h1>Genera imagenes:</h1>
+      <input type={'text'} onChange={(e) => setinputTextImage(e.target.value)} onKeyDown={(handleKeyDownImagen)}/><input type={'button'} value="Enviar" onClick={fetchDataImagen} />
+      {responseImagen !== "" && <><h1> Imagen:</h1><img alt='' src={responseImagen} /></>}
+      </>}
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/LoadingSpinner.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+
+export default function LoadingSpinner() {
+    return (
+        <>
+
+            <div className="spinner-container">
+                <div className="loading-spinner">
+
+                </div>
+
+            </div>
+            <p>
+                Esperando respuesta del servidor
+            </p>
+        </>
+    );
+}
